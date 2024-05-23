e750c546f854f8161f9b1c80d84be13b63367c5c
Francescopiobottaro <piobottaro300403@gmail.com>
2024-05-23 16:41:26 +0200

    id 1 resolve

--- src/main/java/model/ProductModel.java
+++ src/main/java/model/ProductModel.java
@@ -121,8 +121,9 @@ public class ProductModel {
 
 		Collection<ProductBean> products = new LinkedList<ProductBean>();
 
-		String selectSQL = "SELECT * FROM " + ProductModel.TABLE_NAME + " WHERE deleted = 'false' AND nomeTipologia = '" + where + "'";
-		String sql2 = "SELECT AVG(votazione) FROM Recensione WHERE codiceProdotto = ?";
+		String selectSQL = "SELECT * FROM " + ProductModel.TABLE_NAME + " WHERE deleted = 'false' AND nomeTipologia =?"; 
+		String sql2= "SELECT AVG(votazione) FROM recensione WHERE codiceProdotto=?";
+		
 		
 		try {
 			connection = DriverManagerConnectionPool.getConnection();
